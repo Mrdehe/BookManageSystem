e86f886208f226be3886f46cdfb370afd454f517
dehe_zhao <2024015816@st.cupk.edu.cn>
2026-06-30 10:18:01 +0800

    4.7

--- src/main/java/com/cupk/controller/BookController.java
+++ src/main/java/com/cupk/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.cupk.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cupk.common.Result;
 import com.cupk.mapper.BookMapper;
 import com.cupk.pojo.Book;
@@ -14,13 +16,31 @@ public class BookController {
     @Autowired
     private BookMapper bookMapper;
 
+//    @GetMapping("/books")
+//    Result selectAll(){
+//        List<Book> bookList=bookMapper.selectList(null);
+//        for(Book book:bookList){
+//            System.out.println(book);
+//        }
+//        return Result.success(bookList);
+//    }
+
     @GetMapping("/books")
-    Result selectAll(){
-        List<Book> bookList=bookMapper.selectList(null);
-        for(Book book:bookList){
-            System.out.println(book);
-        }
-        return Result.success(bookList);
+    Result selectPages(@RequestParam(defaultValue = "") String name,
+                       @RequestParam(defaultValue = "1") Integer pageNum,
+                       @RequestParam(defaultValue = "5") Integer pageSize){
+        Page<Book> page=new Page<>(pageNum,pageSize);
+        //bookMapper.selectPage(page,null);
+        QueryWrapper<Book> queryWrapper=new QueryWrapper<>();
+        queryWrapper.like("name",name);
+        bookMapper.selectPage(page,queryWrapper);
+
+//        System.out.println("当前页数："+page.getCurrent());
+//        System.out.println("总记录数："+page.getTotal());
+//        System.out.println("总页数："+page.getPages());
+//        System.out.println("是否有下页："+page.hasNext());
+
+        return Result.success(page);
     }
 
     //根据id查询
@@ -73,5 +93,19 @@ public class BookController {
             return Result.error();
         }
     }
+
+    //批量删除
+    @DeleteMapping("/books")
+    Result deleteBatch(@RequestBody List<Integer> ids){
+        int  i =bookMapper.deleteByIds(ids);
+        if(i>0){
+            System.out.println("数据删除成功！");
+            return Result.success();
+        }else {
+            System.out.println("数据删除失败！");
+            return Result.error();
+        }
+    }
+
 
 }
